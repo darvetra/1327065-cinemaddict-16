65c9c3b7dcb074df05396a9a14bb6e491b1414e7
Darvetra <darvetra13@gmail.com>
2021-12-03 05:32:10 +0300

    добавляет модуль сортировка

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import {renderTemplate, RenderPosition} from './render.js';
 
 import {createHeaderProfileTemplate} from './view/header-profile-view';
 import {createMainNavigationTemplate} from './view/main-navigation-view';
+import {createSortTemplate} from './view/sort-view';
 
 const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
 
 renderTemplate(siteHeaderElement, createHeaderProfileTemplate(), RenderPosition.BEFOREEND);
+
+renderTemplate(siteMainElement, createSortTemplate(), RenderPosition.AFTERBEGIN);
 renderTemplate(siteMainElement, createMainNavigationTemplate(), RenderPosition.AFTERBEGIN);
+
