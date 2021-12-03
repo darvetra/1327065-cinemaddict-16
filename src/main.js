8ceec99061ec311b1704b5b2d6956d3a714d9603
Darvetra <darvetra13@gmail.com>
2021-12-03 05:27:45 +0300

    добавляет модуль меню! в тот раз был модуль профиль)

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import {createHeaderProfileTemplate} from './view/header-profile-view';
 import {renderTemplate, RenderPosition} from './render.js';
+
+import {createHeaderProfileTemplate} from './view/header-profile-view';
+import {createMainNavigationTemplate} from './view/main-navigation-view';
 
 const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
 
 renderTemplate(siteHeaderElement, createHeaderProfileTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(siteMainElement, createMainNavigationTemplate(), RenderPosition.AFTERBEGIN);
