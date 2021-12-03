80ef45fdddb055fd89bacc43a57bab081192aa04
Darvetra <darvetra13@gmail.com>
2021-12-03 05:54:48 +0300

    добавляет модуль статистика фильмов

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import {createMainNavigationTemplate} from './view/main-navigation-view';
 import {createSortTemplate} from './view/sort-view';
 import {createFilmsListTemplate} from './view/films-list-view';
 import {createFilmsListExtraTemplate} from './view/films-list-extra-view';
+import {createFooterStatisticsTemplate} from './view/footer-statistics-view';
 
 const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
 const filmsElement = siteMainElement.querySelector('.films');
+const siteFooterStatisticsElement = document.querySelector('.footer__statistics');
 
 // header
 renderTemplate(siteHeaderElement, createHeaderProfileTemplate(), RenderPosition.BEFOREEND);
@@ -20,3 +22,6 @@ renderTemplate(siteMainElement, createMainNavigationTemplate(), RenderPosition.A
 // content
 renderTemplate(filmsElement, createFilmsListTemplate(), RenderPosition.AFTERBEGIN);
 renderTemplate(filmsElement, createFilmsListExtraTemplate(), RenderPosition.BEFOREEND);
+
+// footer
+renderTemplate(siteFooterStatisticsElement, createFooterStatisticsTemplate(), RenderPosition.AFTERBEGIN);
---
+++ src/view/footer-statistics-view.js
@@ -0,0 +1,3 @@
+export const createFooterStatisticsTemplate = () => (
+  '<p>130 291 movies inside</p>'
+);
