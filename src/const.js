26e97d00dff43117a766c8fc49fd84f35b2179c6
Darvetra <darvetra13@gmail.com>
2022-01-13 06:29:41 +0300

    реализовывает отображение состояние эмоции комментария

--- src/const.js
+++ src/const.js
@@ -1,3 +1,12 @@
+export const Emoji = {
+  SMILE: 'smile',
+  SLEEPING: 'sleeping',
+  PUKE: 'puke',
+  ANGRY: 'angry',
+};
+
+export const EMOJIS = Object.values(Emoji);
+
 export const SortType = {
   DEFAULT: 'default',
   DATE: 'date',
