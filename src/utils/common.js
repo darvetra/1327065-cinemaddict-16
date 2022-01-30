1a3e643d0c0a567e0f623df1cce4fc4f0c4ae660
Darvetra <darvetra13@gmail.com>
2022-01-30 18:08:56 +0300

    вносит правки согласно замечаниям наставника

--- src/utils/common.js
+++ src/utils/common.js
@@ -71,10 +71,4 @@ export const getRandomIntInclusive = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
 };
 
-export const isClickOnLink = (evt) => {
-  if (evt.target.tagName !== 'A') {
-    return true;
-  } else {
-    return false;
-  }
-};
+export const isClickOnLink = (evt) => evt.target.tagName !== 'A';
